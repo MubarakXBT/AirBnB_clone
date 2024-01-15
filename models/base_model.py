1f1bf729c0be3e49333479d5903786b6f0d700e1
MubarakXBT <kazeemmubarak@gmail.com>
2024-01-15 05:08:01 +0100

    ================== console.py an inbuilt command interpreter ================== Class storage - To serialize and desiralize object from a json file ==================

--- models/base_model.py
+++ models/base_model.py
@@ -6,43 +6,55 @@ datetime
 """
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
     """
     A Console Module Basemodel
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         instanation of the public instance attributes
         """
         self.id = str(uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.today()
         self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """
         Format how instance object print
         """
-        prnt = "["+__class__.__name__+"] ("\
-            +str(self.id)+") "+str(self.__dict__)
-        return (prnt)
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
         """
         used to update time of last update
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """
-        return dictionar key/value of __dict__ of the instance
+        return dictionar key/value of __dict__ of the Models instance
         """
 
-        return {"my_number": self.my_number,
-                "name": self.name,
-                "__class__": __class__.__name__,
-                "updated_at": self.updated_at.isoformat(),
-                "id": self.id,
-                "created_at": self.created_at.isoformat(),
-                }
+        result_dict = self.__dict__.copy()
+        if (type(self.created_at) == str):
+            result_dict["created_at"] = self.created_at
+        else:
+            result_dict["created_at"] = self.created_at.isoformat()
+
+        if (type(self.created_at) == str):
+            result_dict["updated_at"] = self.updated_at
+        else:
+            result_dict["updated_at"] = self.updated_at.isoformat()
+        result_dict["__class__"] = self.__class__.__name__
+        return result_dict
